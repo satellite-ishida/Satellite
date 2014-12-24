0b00aa85dd2969d31d3586761fcbc1743cdce140
uhy-uhy <uhy.g62@gmail.com>
2014-12-24 13:01:27 +0900

    微変更

--- Assets/Node_Info.cs
+++ Assets/Node_Info.cs
@@ -14,15 +14,16 @@ public class Node_Info : MonoBehaviour {
         ID = sc.ID;
     }
 
+    //リスト中のノードをクリックしたとき
     public void Click_Sat_Node()
     {
         Node_Info si = gameObject.GetComponent<Node_Info>();
         Info_Panel_Manager.target_Change(si.ID);
+        SatelliteComponent sc_new = GameMaster.GetSatelliteByID(ID).GetComponent<SatelliteComponent>();
 
         if (GUI_Manager.SensorON_ID != ID)
         {
             SatelliteComponent sc_old = GameMaster.GetSatelliteByID(GUI_Manager.SensorON_ID).GetComponent<SatelliteComponent>();
-            SatelliteComponent sc_new = GameMaster.GetSatelliteByID(ID).GetComponent<SatelliteComponent>();
 
             if (sc_old.sensorOn)
             {
@@ -36,11 +37,9 @@ public class Node_Info : MonoBehaviour {
         }
         else if (GUI_Manager.SensorON_ID == ID)
         {
-            SatelliteComponent sc_new = GameMaster.GetSatelliteByID(ID).GetComponent<SatelliteComponent>();
             sc_new.Sensor_Switch();
         }
 
         GUI_Manager.SensorON_ID = ID;
-
     }
 }
